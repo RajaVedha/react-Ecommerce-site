dcfe4bba46854c05eed8a3a1392438e463a9b052
raja vedha <bandiraja2018@gmail.com>
2023-05-26 17:08:40 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
       <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="/react-Ecommerce-site" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
