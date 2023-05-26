bb9e6253ecfd32417c01cf3bcf2ba75bd50862d9
raja vedha <bandiraja2018@gmail.com>
2023-05-26 16:18:35 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,44 @@
+import product1 from './assets/10.webp';
+import product2 from './assets/11.png';
+import product3 from './assets/12.png';
+import product4 from './assets/13.png';
+import product5 from './assets/14.png';
+import product6 from './assets/14pro.png';
+
+export const PRODUCTS = [ {
+    id: 1,
+    productName: "Tshirt",
+    price: 50000,
+    productImage:product1
+},
+{
+    id: 2,
+    productName: "Ledstrip",
+    price: 60000,
+    productImage:product2
+},
+{
+    id: 3,
+    productName: "Jacket",
+    price: 70000,
+    productImage:product3
+},
+{
+    id: 4,
+    productName: "Camera",
+    price: 90000,
+    productImage:product4
+},
+{
+    id: 5,
+    productName: "Iphone",
+    price: 30000,
+    productImage:product5
+},
+{
+    id: 6,
+    productName: "Laptop",
+    price: 10000,
+    productImage:product6
+}
+];
